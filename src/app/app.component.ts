c55ad0865b388d1fe33ed05f0a08b28cd1fdb942
Barış KARAKAYA <181805054@ofis365.adu.edu.tr>
2021-03-12 17:59:58 +0300

    Angular ile Frontend'e Yolculuk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ɵɵstylePropInterpolate5 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+//{} = obje
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'northwind';
+  user: string = "Barış"
+  
 }
